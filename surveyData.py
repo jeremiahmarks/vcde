50511a16c25aa7b4fdc935d9db07c272ed67e424
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-17 19:43:11 -0700

    First example to get as CSV completed
    using requests I was able to get the file as a CSV

--- surveyData.py
+++ surveyData.py
@@ -5,3 +5,16 @@
 #Eventually this will break the SRs up by the store number and, perhaps add
 #controls which control IE to page through the options. 
 
+import requests
+
+import urllib.request
+
+temporaryTestingURL="https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/edit?usp=sharing"
+csvTailer = "&output=csv"
+with urllib.request.urlopen(temporaryTestingURL + csvTailer) as response:
+	html = response.read()
+
+n1 = "https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/export?format=csv"
+r=requests.get(n1)
+r
+r.text
