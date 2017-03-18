ba595ff47ae0bd3812632cc9c7fed23ece7abcaa
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-17 18:22:31 -0700

    Added SurveyData with base comments
    Created file and started with comments.

---
+++ surveyData.py
@@ -0,0 +1,7 @@
+#surveyData will exist to accept a URL and then get the data for 
+#surveys assigned to me. It will output a single search string
+#with all assigned SRs ORd together.
+
+#Eventually this will break the SRs up by the store number and, perhaps add
+#controls which control IE to page through the options. 
+
