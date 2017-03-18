b6216a394b8fb34fbb5e26633053a904fedfefcd
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-18 00:59:28 -0700

    Comple file download start with csv
    Completed portion that determines the document path and downloads
    document. After document is downloaded I am needing to save and reload
    it, unsure as to why that is. sorted out my surveys from the list,
    sorted the surveys by store. Need to format information so that it is
    useful. Need to sort the list and assign based on grouping by oldest.

--- surveyData.py
+++ surveyData.py
@@ -5,16 +5,43 @@
 #Eventually this will break the SRs up by the store number and, perhaps add
 #controls which control IE to page through the options. 
 
+# temporaryTestingURL="https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/edit?usp=sharing"
 import requests
+import csv
+from collections import defaultdict 
 
-import urllib.request
+global CSVSuffix
+global fileStoragePath
 
-temporaryTestingURL="https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/edit?usp=sharing"
-csvTailer = "&output=csv"
-with urllib.request.urlopen(temporaryTestingURL + csvTailer) as response:
-	html = response.read()
+spreadsheetURL = ""
+CSVSuffix = "export?format=csv"
+fileStoragePath = "C:////Users//Jeremiah//Desktop//tout.csv"
 
-n1 = "https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/export?format=csv"
-r=requests.get(n1)
-r
-r.text
+
+def getDocument():
+	spreadsheetURL = str(raw_input("Please enter the URL:\n"))
+	lastSlash = spreadsheetURL.rfind("/")
+	csvDownloadUrl = spreadsheetURL[:lastSlash+1] + CSVSuffix
+	surveyData = requests.get(csvDownloadUrl)
+	with open(fileStoragePath, 'wb') as outfile:
+		outfile.write(a.text)
+		#In case you're wondering, "with" closes the file for me
+	with open(fileStoragePath, 'rb') as infile:
+		csvReader = csv.DictReader(infile)
+		mysurveys = []
+		for eachline in csvReader:
+			if eachline['Name'] == 'Jeremiah':
+				mysurveys.append(eachline)
+	surveysByStore = defaultdict(list)
+	for eachline in mysurveys:
+		surveysByStore[eachline['Site #']].append(eachline)
+	for eachstore in surveysByStore.keys():
+		pass
+	return surveysByStore
+
+
+
+# n1 = "https://docs.google.com/spreadsheets/d/1ELFMvDBhuROF9nMKpvMHQT8jLwOuvDIOUy1_hxYsda8/export?format=csv"
+# r=requests.get(n1)
+# r
+# r.text
