8d1ea31a4fd66d99327445367bb653eb8be244ec
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-22 23:07:45 -0700

    Add useful methods and functions

--- getSpreadsheet.py
+++ getSpreadsheet.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: jemarks
 # @Date:   2017-03-21 17:48:30
-# @Last Modified by:   jemarks
-# @Last Modified time: 2017-03-22 20:02:24
+# @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2017-03-22 23:05:48
 
 #This script will find the most recent spreadsheet with 
 #needed surveys and then download it to the users desktop
@@ -20,6 +20,15 @@ import glob
 import datetime
 import pandas
 
+#Provide strings to represent path to file and parsing instructions
+fileSearchString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations/Jobscomplete_Wo_Survey_Scottsdale_*"
+strpFormatString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations\\Jobscomplete_Wo_Survey_Scottsdale_%m.%d.%Y_at_%H.%M.xlsx"
+
+#Provide list of needed and existing columns
+columnsToAdd = ["Name", "Completed", "Siebel Search String"]
+columnsInOriginalFile = ["SR Num", "Site #", "State", "Time Zone", "LOS", "Days passed since Completed", "Caller Name", "CRM"]
+allColumns = columnsToAdd + columnsInOriginalFile
+
 
 class surveyLineItem():
 	"""The surveyLineItem class is designed to parse text into meaningful
@@ -35,10 +44,26 @@ class surveyLineItem():
 			self.daysSinceComplete = int(lineData['Days passed since Completed'])
 		except Exception as e:
 			self.daysSinceComplete = lineData['Days passed since Completed']
-
-		
 		self.caller = lineData['Caller Name']
 		self.crm = lineData['CRM']
+		self.name=""
+		self.completed=""
+		self.searchString=""
+
+	def getCSVRepresentation(self):
+		csvReturn = {}
+		csvReturn['SR Num'] = str(self.sr)
+		csvReturn['Site #'] = str(self.siteNumber)
+		csvReturn['State'] = str(self.state)
+		csvReturn['Time Zone'] = str(self.tz)
+		csvReturn['LOS'] = str(self.los)
+		csvReturn['Days passed since Completed'] = str(self.daysSinceComplete)
+		csvReturn['Caller Name'] = str(self.caller)
+		csvReturn['CRM'] = str(self.crm)
+		csvReturn['Name'] = str(self.name)
+		csvReturn['Completed'] = ""
+		csvReturn['searchString'] = self.searchString
+		return csvReturn
 
 
 class PetmLocation():
@@ -47,18 +72,27 @@ class PetmLocation():
 	"""
 	def __init__(self, siteNumber):
 		self.siteNum = siteNumber
-		self.oldestSR = datetime.date.today()
+		self.oldestSR = 0
 		self.allSRs = []
 
 	def addSurvey(self, surveyLineItem):
-		if surveyLineItem.dateComplete < self.oldestSR:
-			self.oldestSR = surveyLineItem.dateComplete
+		if surveyLineItem.daysSinceComplete > self.oldestSR:
+			self.oldestSR = surveyLineItem.daysSinceComplete
 		self.allSRs.append(surveyLineItem)
 
 	def getSearchString(self):
 		return " OR ".join([sr.sr for sr in self.allSRs])
 
-class surveyPresenter():
+	def getCSVLines(self):
+		storesCSVLines = []
+		searchString = self.getSearchString()
+		for eachsr in self.allSRs:
+			eachsr.searchString = searchString
+			storesCSVLines.append(eachsr.getCSVRepresentation())
+		return storesLines
+
+
+class vixxoCSR():
 	"""This class will hold which surveys who is doing.
 	"""
 	def __init__(self, name, maxSurveys=10):
@@ -74,24 +108,27 @@ class surveyPresenter():
 		pass
 
 
-fileSearchString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations/Jobscomplete_Wo_Survey_Scottsdale_*"
-strpFormatString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations\\Jobscomplete_Wo_Survey_Scottsdale_%m.%d.%Y_at_%H.%M.xlsx"
-
-columnsToAdd = ["Name", "Completed"]
-columnsInOriginalFile = ["SR Num", "Site #", "State", "Time Zone", "LOS", "Days passed since Completed", "Caller Name", "CRM"]
-
-allColumns = columnsToAdd + columnsInOriginalFile
 
 
 #First Thing - Find the most recent file
-matchingFiles = glob.glob(fileSearchString)
+def getNewestFile():
+	#This will return a list of dicts, basically like
+	#using csv.DictReader to read into a list.
+	matchingFiles = glob.glob(fileSearchString)
 
-newestFile = matchingFiles[0]
-for eachfile in matchingFiles:
-	if datetime.datetime.strptime(eachfile, strpFormatString) > datetime.datetime.strptime(newestFile, strpFormatString):
-		newestFile = eachfile
+	newestFile = matchingFiles[0]
+	for eachfile in matchingFiles:
+		if datetime.datetime.strptime(eachfile, strpFormatString) > datetime.datetime.strptime(newestFile, strpFormatString):
+			newestFile = eachfile
 
-surveysFile = pandas.read_excel(newestFile).to_dict(orient='records')
+	surveysFile = pandas.read_excel(newestFile).to_dict(orient='records')
+
+	return surveysFile
+
+def trimDataAndGroup(listOfCSVLines):
+	for eachline in listOfCSVLines:
+		thisline = surveyLineItem(eachline)
+
 
 allRows = []
 for eachrow in surveysFile:
