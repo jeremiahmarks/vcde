40ea6c2e73990be9913331dff651b57e596db42a
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-22 19:50:16 -0700

    Added basic outline to getSpreadsheet
    Started building out the file finding and reformatting logic

--- getSpreadsheet.py
+++ getSpreadsheet.py
@@ -2,7 +2,7 @@
 # @Author: jemarks
 # @Date:   2017-03-21 17:48:30
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-03-22 12:19:14
+# @Last Modified time: 2017-03-22 19:49:29
 
 #This script will find the most recent spreadsheet with 
 #needed surveys and then download it to the users desktop
@@ -12,4 +12,30 @@
 #
 #folder path
 	#//bos-mart.ip-tech.com/FSNPublishedReports/Operations/
+
+
+
+#glob exists to match file names
+import glob
+import datetime
+import pandas
+
+fileSearchString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations/Jobscomplete_Wo_Survey_Scottsdale_*"
+strpFormatString = "//bos-mart.ip-tech.com/FSNPublishedReports/Operations\\Jobscomplete_Wo_Survey_Scottsdale_%m.%d.%Y_at_%H.%M.xlsx"
+
+columnsToAdd = ["Name", "Completed"]
+columnsInOriginalFile = ["SR Num", "Site #", "State", "Time Zone", "LOS", "Days passed since Completed", "Caller Name", "CRM"]
+
+allColumns = columnsToAdd + columnsInOriginalFile
+
+
+#First Thing - Find the most recent file
+matchingFiles = glob.glob(fileSearchString)
+
+newestFile = matchingFiles[0]
+for eachfile in matchingFiles:
+	if datetime.datetime.strptime(eachfile, strpFormatString) > datetime.datetime.strptime(newestFile, strpFormatString):
+		newestFile = eachfile
+
+surveysFile = pandas.read_excel(newestFile).to_dict(orient='records')
 
