cf46bbff17ceb15a33fa45ba254388c82aabcf71
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-22 18:56:46 -0700

    Created getSpreadsheet
    This will basically be a more real proof of concept.

---
+++ getSpreadsheet.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: jemarks
+# @Date:   2017-03-21 17:48:30
+# @Last Modified by:   jemarks
+# @Last Modified time: 2017-03-22 12:19:14
+
+#This script will find the most recent spreadsheet with 
+#needed surveys and then download it to the users desktop
+#
+#Filename template:
+	#Jobscomplete_Wo_Survey_Scottsdale_MM.DD.YYYY_at_HH.MM.xlsx
+#
+#folder path
+	#//bos-mart.ip-tech.com/FSNPublishedReports/Operations/
+
