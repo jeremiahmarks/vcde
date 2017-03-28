861a7a7b00ed0eec8330dadf07ef21e24354dc6a
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-27 21:45:58 -0700

    Add email subject as string (not yet implemented) Describe process to user

--- dist/start.py
+++ dist/start.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Jeremiah
 # @Date:   2017-03-25 15:23:07
-# @Last Modified by:   jemarks
-# @Last Modified time: 2017-03-27 18:50:06
+# @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2017-03-27 21:45:14
 
 import googleAuth
 import logicHandler
@@ -11,24 +11,29 @@ import os
 
 def main():
 	newDocTitle = "Survey Assignments " + datetime.date.strftime(datetime.date.today(), '%d%B%Y')
+	newEmailSubject = datetime.date.strftime(datetime.date.today(), '%d%B%Y') + " Survey Assignments"
 
-	print ("Get Agents")
+	print("Get Agents")
 	agents = logicHandler.getAgents()
-	print ("Get and Trim newest files")
+	print("Get newest files")
 	surveysFile = logicHandler.getNewestFile()
+	print("Trim and group")
 	surveysFile = logicHandler.trimDataAndGroup(surveysFile)
 
-	print ("Collect Stats")
+	print("Collect Stats")
 	totalSRs, totalOldSRs = logicHandler.collectEmailStats(surveysFile)
 
-	print ("Assign Surveys")
+	print("Assign Surveys")
 	surveysFile, agents = logicHandler.assignSurveys(surveysFile, agents)
 	csvFile = logicHandler.combine_to_csv(surveysFile, agents)
 
-	print ("upload to google")
+	print("upload to google")
 	sheetService = googleAuth.getSheetService()
+	print("create spreadsheet")
 	newSpreadsheet = googleAuth.addSpreadsheet(sheetService, title=newDocTitle)
+	print("update spreadsheet values")
 	result_of_adding = googleAuth.add_lines_to_spreadsheet(sheetService, newSpreadsheet['spreadsheetId'], csvFile)
+	print("change spreadsheet privacy")
 	result_of_sharing = googleAuth.shareDocument(newSpreadsheet['spreadsheetId']).execute()
 	sheetURL = newSpreadsheet['spreadsheetUrl']
 
@@ -36,11 +41,6 @@ def main():
 	print("Create Email")
 	logicHandler.createEmail(totalSRs, totalOldSRs, sheetURL)
 
-	# print (emailBody)
-	# emailFilename = datetime.date.strftime(datetime.date.today(), '%d%B%Y') + "SurveyEmail.txt"
-	# emailFilePath = os.path.join(logicHandler.vixxoUploadDirectory, emailFilename)
-	# with open(emailFilePath, 'w') as outfile:
-	# 	outfile.write(emailBody)
 
 if __name__ == '__main__':
 	main()
