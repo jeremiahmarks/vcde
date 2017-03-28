50083cb535ac26547a2dbee44c6221654cb4169a
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-27 18:59:26 -0700

    Update googleAuth for correct path to client secret.

--- dist/googleAuth.py
+++ dist/googleAuth.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Jeremiah
 # @Date:   2017-03-24 22:56:46
-# @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2017-03-25 16:33:27
+# @Last Modified by:   jemarks
+# @Last Modified time: 2017-03-27 18:51:57
 
 
 from __future__ import print_function
@@ -23,7 +23,7 @@ except ImportError:
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
-CLIENT_SECRET_FILE = 'C:\\Users\\Jeremiah\\Desktop\\client_secret.json'
+CLIENT_SECRET_FILE = 'C:\\Users\\jemarks\\Desktop\\VixxoCode\\VixxoCode\\clientid.json'
 APPLICATION_NAME = 'vixxoUploader'
 
 
