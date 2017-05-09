f768a4fed15329aa1ebe65f1e416a6eb53e12169
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-08 18:04:14 -0700

    Add settings file.
    Basically making sure it can be set up.

---
+++ EmailMcAutoEmail/auto_settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: jemarks
+# @Date:   2017-05-08 18:03:14
+# @Last Modified by:   jemarks
+# @Last Modified time: 2017-05-08 18:03:42
+
+settings = {}
+settings["TL"] = "JTRAVERS"
