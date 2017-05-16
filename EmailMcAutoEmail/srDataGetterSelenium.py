0e1bb4760cc39148d887c49e29890e1749bbae82
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-15 19:55:25 -0700

    Add method to get SR details for Selenium.
    Additionally notate which elements need to be retrieved.

--- EmailMcAutoEmail/srDataGetterSelenium.py
+++ EmailMcAutoEmail/srDataGetterSelenium.py
@@ -2,7 +2,7 @@
 # @Author: jemarks
 # @Date:   2017-05-12 19:28:29
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-12 20:00:06
+# @Last Modified time: 2017-05-15 19:54:02
 
 # This class exists because our website uses silly ajax for the
 # internal notes and robobrowser does not deal with it well.
@@ -27,3 +27,28 @@ class SRScraper(object):
 		element.send_keys(password)
 		element.send_keys(selenium.webdriver.common.keys.Keys.ENTER)
 		
+	def get_sr_details(self, srnumber):
+		sr_url = 'https://myfsn.biz/SC_Main/SC_SRDetail.aspx?AllowAnyTech=Y&srid=' + srnumber
+		self.driver.get(sr_url)
+		checkbox_id = 'ctl00_ContentPlaceHolder1_systemNotesCheckbox'
+		self.driver.find_elements_by_id(checkbox_id).send_keys(webdriver.common.keys.Keys.SPACE)
+		# Some data to specifically get:
+		# Site number
+		# Site Limit
+		# City
+		# State
+		# Related SR(s)
+		# Site Number
+		# Status per myfsn
+		# Type/Line of service
+		# Reason for call
+		# Priority
+		# Site Area
+		# Site Sub Area
+		# Date Opened
+		# ETAs and SLA data
+		# Description
+		# Special Instruction
+		# SP
+		# All the notes. 
+		# Need all elements of class infoGridRow, infoRow, and infoRowFull
