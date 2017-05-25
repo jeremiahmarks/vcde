67cefce16767125d284f7c0fcd0226863dc7ea8d
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-25 11:07:55 -0700

    Add substatus and ability to filter by them
    Additionally break remote file names into components for remote path and
    file name attributes.

--- EmailMcAutoEmail/start.py
+++ EmailMcAutoEmail/start.py
@@ -2,7 +2,7 @@
 # @Author: jemarks
 # @Date:   2017-05-08 17:40:30
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-24 15:32:56
+# @Last Modified time: 2017-05-25 11:06:40
 
 import glob
 import datetime
@@ -18,11 +18,16 @@ import win32com.client as win32
 from auto_settings import settings
 
 # Provide strings to represent path to file and parsing instructions
-fileSearchString = "\\\\bos-mart.ip-tech.com\\FSNPublishedReports\\ITDevelopment\\Petsmart_Backstop_*"
-strpFormatString = "\\\\bos-mart.ip-tech.com\\FSNPublishedReports\\ITDevelopment\\Petsmart_Backstop_%m-%d-%Y_at_%H.%M.xlsx"
+fileSearchString = "Petsmart_Backstop_*"
+remoteDirectory = "\\\\bos-mart.ip-tech.com\\FSNPublishedReports\\ITDevelopment\\"
+strpFormatString = "Petsmart_Backstop_%m-%d-%Y_at_%H.%M.xlsx"
+
+remotefileSearchString = remoteDirectory + fileSearchString
+remotestrpFormatString = remoteDirectory + strpFormatString
 
 
-
+status_column="SR Substatus"
+negative_statuses=["No ETA", "Quote Required", "Quote Received", "New ETA Required", "No Time Out", "Job Not Complete", "Quoted - Approved by Customer", "Unassigned", "Assigned to Service Contractor"]
 
 home_dir = os.path.expanduser('~')
 BS_Folder = os.path.join(home_dir, 'BS_report')
@@ -37,11 +42,11 @@ colsInOrder = ["SR #", "Date Opened", "Days Since Open", "Priority", "SR Substat
 def getNewestFile():
 	#This will return a list of dicts, basically like
 	#using csv.DictReader to read into a list.
-	matchingFiles = glob.glob(fileSearchString)
+	matchingFiles = glob.glob(remotefileSearchString)
 
 	newestFile = matchingFiles[0]
 	for eachfile in matchingFiles:
-		if datetime.datetime.strptime(eachfile, strpFormatString) > datetime.datetime.strptime(newestFile, strpFormatString):
+		if datetime.datetime.strptime(eachfile, remotestrpFormatString) > datetime.datetime.strptime(newestFile, remotestrpFormatString):
 			newestFile = eachfile
 
 	bs_file = pandas.read_excel(newestFile, sheetname=1).to_dict(orient='records')
@@ -53,11 +58,11 @@ def getNewestFile():
 def newer_file_exists():
 	#This will return a list of dicts, basically like
 	#using csv.DictReader to read into a list.
-	matchingFiles = glob.glob(fileSearchString)
+	matchingFiles = glob.glob(remotefileSearchString)
 
 	newestFile = matchingFiles[0]
 	for eachfile in matchingFiles:
-		if datetime.datetime.strptime(eachfile, strpFormatString) > datetime.datetime.strptime(newestFile, strpFormatString):
+		if datetime.datetime.strptime(eachfile, remotestrpFormatString) > datetime.datetime.strptime(newestFile, remotestrpFormatString):
 			newestFile = eachfile
 	newestFileName = os.path.basename(newestFile)
 	newestFileFullPath = os.path.join(BS_Folder, newestFileName)
@@ -82,7 +87,7 @@ def getSpecificFile(filename):
 def email_Travers(local_file):
 	localData = pandas.read_excel(local_file).to_dict(orient='records')
 	travers_file = pandas.DataFrame([row for row in localData if row['TL'] == "JTRAVERS"])
-	criticals = pandas.DataFrame([row for row in localData if (row['TL'] == "JTRAVERS" and row['Priority'] == "Critical")]).sort_values(by="Days Since Open")
+	criticals = pandas.DataFrame([row for row in localData if (row['TL'] == "JTRAVERS" and row['Priority'] == "Critical" and row[status_column] in negative_statuses)]).sort_values(by="Days Since Open")
 	local_dir = os.path.dirname(local_file)
 	origFile = os.path.basename(local_file)
 	travers_filename = 'JTRAV' + origFile[:origFile.rfind('.')] + origFile[origFile.rfind('.'):]
@@ -100,7 +105,7 @@ def email_Travers(local_file):
 def email_LZ(local_file):
 	localData = pandas.read_excel(local_file).to_dict(orient='records')
 	lelz_file = pandas.DataFrame([row for row in localData if row['TL'] == "LELZIE"])
-	criticals = pandas.DataFrame([row for row in localData if (row['TL'] == "LELZIE" and row['Priority'] == "Critical")]).sort_values(by="Days Since Open")
+	criticals = pandas.DataFrame([row for row in localData if (row['TL'] == "LELZIE" and row['Priority'] == "Critical" and row[status_column] in negative_statuses)]).sort_values(by="Days Since Open")
 	local_dir = os.path.dirname(local_file)
 	origFile = os.path.basename(local_file)
 	lelz_filename = 'LELZ' + origFile[:origFile.rfind('.')] + origFile[origFile.rfind('.'):]
