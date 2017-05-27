2807a8e911d91d63fb5882f86779fb8f87d96b5f
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 20:39:28 -0700

    change newFile to new_file

--- backstop_automation/runservice.py
+++ backstop_automation/runservice.py
@@ -2,7 +2,7 @@
 # @Author: jemarks
 # @Date:   2017-05-26 18:20:32
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 20:36:42
+# @Last Modified time: 2017-05-26 20:39:03
 
 import time
 import ctypes
@@ -22,7 +22,7 @@ def main():
 			new_file = file_man.check_for_new()
 			if new_file:
 				local_file = file_man.get_specific_file(new_file)
-				actions_man.newFile(local_file)
+				actions_man.new_file(local_file)
 			if is_locked():
 				mouse_man.jiggle()
 		except Exception as e:
