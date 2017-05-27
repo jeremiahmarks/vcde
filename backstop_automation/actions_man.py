d6d2bc02f2896aad12af265ac10e436a676b4597
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 21:02:46 -0700

    Add simple body to actions_man.new_file

--- backstop_automation/actions_man.py
+++ backstop_automation/actions_man.py
@@ -2,4 +2,13 @@
 # @Author: jemarks
 # @Date:   2017-05-26 20:37:38
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 20:37:38
+# @Last Modified time: 2017-05-26 20:40:31
+
+# This file will house all of the various actions to take when there is a new file
+
+
+def new_file(local_file):
+	"""This is the action called when there is a new file.
+	It is basically the method used to generate all of the email stuff.
+	"""
+	print(local_file)
