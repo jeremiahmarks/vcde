340657373b0259914bc6ce45ceb7c850d9f98811
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 20:37:02 -0700

    Add method to check if screen is locked or not.

--- backstop_automation/runservice.py
+++ backstop_automation/runservice.py
@@ -2,13 +2,19 @@
 # @Author: jemarks
 # @Date:   2017-05-26 18:20:32
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 18:43:22
+# @Last Modified time: 2017-05-26 20:36:42
 
 import time
+import ctypes
 
 from . import file_man
 from . import actions_man
 from . import mouse_man
+
+user = ctypes.windll.User32
+
+def is_locked():
+	return user.GetForegroundWindow() == 0
 
 def main():
 	while True:
@@ -17,7 +23,8 @@ def main():
 			if new_file:
 				local_file = file_man.get_specific_file(new_file)
 				actions_man.newFile(local_file)
-			mouse_man.jiggle()
+			if is_locked():
+				mouse_man.jiggle()
 		except Exception as e:
 			print(e)
 	finally:
