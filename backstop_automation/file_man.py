7d86eb936b2dd38cbf506655fd90f7ea4d7b7fdd
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 21:37:39 -0700

    Add method to get sorted list of local and remote files

--- backstop_automation/file_man.py
+++ backstop_automation/file_man.py
@@ -2,7 +2,7 @@
 # @Author: jemarks
 # @Date:   2017-05-26 18:42:49
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 19:53:47
+# @Last Modified time: 2017-05-26 21:36:53
 
 # Methods I currently am using elsewhere
 # file_man.check_for_new()
@@ -24,12 +24,17 @@ BS_Folder = os.path.join(home_dir, 'BS_report')
 if not os.path.exists(BS_Folder):
 	os.makedirs(BS_Folder)
 
+def get_remote_files():
+	return sorted(glob.glob(os.path.join(remote_directory, file_search_string)), key = lambda x: datetime.datetime.strptime(x, os.path.join(remote_directory, sftp_format_string)), reverse=True)
+
+def get_local_files():
+	return sorted(glob.glob(os.path.join(BS_Folder, file_search_string)), key = lambda x: datetime.datetime.strptime(x, os.path.join(BS_Folder, sftp_format_string)), reverse=True)
 
 def check_for_new():
 	# If there is a new file, this will return the
 	# path to the file. If there is not it will 
 	# return None
-	newest_file = sorted(glob.glob(os.path.join(remote_directory, file_search_string)), key = lambda x: datetime.datetime.strptime(x, os.path.join(remote_directory, sftp_format_string)), reverse=True)[0]
+	newest_file = get_remote_files()[0]
 	newest_file_name = os.path.basename(newest_file)
 	newest_file_local_path = os.path.join(BS_Folder, newest_file_name)
 	if not (os.path.exists(newest_file_local_path)):
@@ -42,3 +47,5 @@ def get_specific_file(file_path):
 	local_file_name = os.path.basename(file_path)
 	local_file_path = os.path.join(BS_Folder, local_file_name)	
 	shutil.copyfile(file_path, local_file_path)
+
+
