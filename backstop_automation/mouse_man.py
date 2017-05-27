f54b34c6a99635920205ee84894e6beef94bc6da
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 21:02:12 -0700

    Change mouseman to use ctypes

--- backstop_automation/mouse_man.py
+++ backstop_automation/mouse_man.py
@@ -2,21 +2,20 @@
 # @Author: jemarks
 # @Date:   2017-05-26 20:17:24
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 20:29:53
+# @Last Modified time: 2017-05-26 21:01:32
 
 # This module exists to simply jiggle the mouse
-import win32api
+import ctypes
 import time
-import math
 
+mouse_event = ctypes.windll.user32.mouse_event
+MOUSEEVENTF_MOVE = 0x0001
 def jiggle():
-	"""This method exists to jiggle the mouse"""
+	print("hello!")
+	mouse_event(MOUSEEVENTF_MOVE, 0, 0, 0, 0)
 
-	for i in range(500):
-		x = int(500+math.sin(math.pi*i/100)*500)
-		y = int(500+math.cos(i)*100)
-		win32api.SetCursorPos((x,y))
-		time.sleep(.01)
 
 if __name__ == '__main__':
-	jiggle()
+	while True:
+		jiggle()
+		time.sleep(3)
