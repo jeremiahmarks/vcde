d4446864f9707bc1454384272e90c9a91f1d27bf
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-27 02:16:48 -0700

    Move team structure to its own file

---
+++ backstop_automation/petm_teams.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Author: Jeremiah
+# @Date:   2017-05-27 02:10:16
+# @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2017-05-27 02:16:02
+
+teams = {}
+
+teams["HFELICIANO"] = []
+teams["JTRAVERS"] = []
+teams["KHUNT"] = []
+teams["KVANNESS"] = []
+teams["LELZIE"] = []
+teams["MZEPEDA"] = []
+teams["SCATRONE"] = []
+
+
+teams['JTRAVERS'].append('Jennifer.Travers@vixxo.com')
+teams['JTRAVERS'].append('Devon.Mix@vixxo.com')
+teams['JTRAVERS'].append('Michael.Lugo@vixxo.com')
+teams["JTRAVERS"].append('Jeremiah.Marks@vixxo.com')
+
+teams['LELZIE'].append('Laron.Elzie@vixxo.com')
+teams['LELZIE'].append('Kyle.Buggs@vixxo.com')
+teams['LELZIE'].append('Robert.Cottrell@vixxo.com')
+teams['LELZIE'].append('Nathan.Wu@vixxo.com')
+teams["LELZIE"].append('Jeremiah.Marks@vixxo.com')
+
+teams["HFELICIANO"].append('Jeremiah.Marks@vixxo.com')
+
+teams["KHUNT"].append('Jeremiah.Marks@vixxo.com')
+
+teams["KVANNESS"].append('Jeremiah.Marks@vixxo.com')
+
+teams["MZEPEDA"].append('Jeremiah.Marks@vixxo.com')
+
+teams["SCATRONE"].append('Jeremiah.Marks@vixxo.com')
+
