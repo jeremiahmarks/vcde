8c6c11ec9da0c105b9d0b1efb97eaa39cc75f7ed
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-26 18:42:23 -0700

    Write main function for runservice
    I know that I am doing this backwards, but this seems to make sense.  I
    need those functions in order for the thing to run, so I need to provide
    those functions.

--- backstop_automation/runservice.py
+++ backstop_automation/runservice.py
@@ -2,4 +2,23 @@
 # @Author: jemarks
 # @Date:   2017-05-26 18:20:32
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-26 18:20:32
+# @Last Modified time: 2017-05-26 18:41:21
+
+import time
+
+def main():
+	while True:
+		try:
+			new_file = file_man.check_for_new()
+			if new_file:
+				local_file = file_man.get_specific_file(new_file)
+				actions_man.newFile(local_file)
+			mouse_man.jiggle()
+		except Exception as e:
+			print(e)
+	finally:
+		time.sleep(300)
+
+if __name__ == '__main__':
+	main()
+
