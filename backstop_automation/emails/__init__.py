4cb633dea765d8ee8463632afc2c422c70dd2a18
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-30 09:07:37 -0700

    add basic method to generate email body.

---
+++ backstop_automation/emails/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Jeremiah
+# @Date:   2017-05-29 19:35:11
+# @Last Modified by:   Jeremiah
+# @Last Modified time: 2017-05-29 19:35:11
---
+++ backstop_automation/emails/team_email.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Jeremiah
+# @Date:   2017-05-29 19:35:43
+# @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2017-05-29 19:37:28
+
+
+def body(all_stats, all_crits, tl_stats, tl_crits):
+	"""Nope, I need to compare the last one, at least. 
+	"""
