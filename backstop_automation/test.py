f8eccf88e4650328d1da9ae8d85102d388d92421
Jeremiah Marks <jeremiah@jlmarks.org>
2017-05-31 18:36:45 -0700

    Change test to test more current methods.

--- backstop_automation/test.py
+++ backstop_automation/test.py
@@ -2,17 +2,20 @@
 # @Author: jemarks
 # @Date:   2017-05-26 20:41:48
 # @Last Modified by:   jemarks
-# @Last Modified time: 2017-05-30 14:39:44
+# @Last Modified time: 2017-05-31 17:11:51
 
 import ctypes
 import time
 import file_man
 import actions_man
+from backstop_objects import BackStopReport
+
 
 
 def main():
-	newFile = file_man.get_local_files()[0]
-	actions_man.new_file(newFile)
+	file_man.get_all_remote_files()
+	newest_local = file_man.get_local_files()[0]
+	actions_man.new_file(newest_local)
 
 if __name__ == '__main__':
 	main()
